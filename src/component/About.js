ec91eeeb9b5ac505ec7f50cb8e43882d71c6a483
SidAbhi <megamancodenamezerobytes@gmail.com>
2021-05-08 00:10:50 +0700

    fixed some css

--- src/component/About.js
+++ src/component/About.js
@@ -11,7 +11,7 @@ function About () {
         <h2 className="AboutMeTitle">ABOUT ME</h2>
         <div className="AboutMeContent">
           <p>Hello. I'm Siddhi, a design focused frontend developer. With 10 years of graphic design experience, primarily focused on motion and layout design, I became interested in web and frontend development in the past year. </p>
-          <p>The primary draw of web design and frontend development for me is the interactivity and the UI/UX aspect, with the freedom to design, build, and create experiences without the restrictions of traditional media.</p>
+          <p>The primary draw for me is the interactivity and the UI/UX aspect of web, with the freedom to design, build, and create experiences without the restrictions of traditional media.</p>
         </div>
       </div>
     </div>
