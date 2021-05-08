822ba8d49cdcc4d045dcf09e507066d0bcad9972
SidAbhi <megamancodenamezerobytes@gmail.com>
2021-05-08 13:36:26 +0700

    no message

--- src/component/About.js
+++ src/component/About.js
@@ -1,17 +1,39 @@
 import React from "react";
-import { useSpring, animated, useTrail } from "react-spring";
+import { useSpring, animated } from "react-spring";
 import { useScroll } from "react-use-gesture";
 import { useMediaQuery } from "react-responsive";
+import useWindowDimensions from "./GetWindowDimensions"
 import "../stylesheets/About.css"
 
 function About () {
+  const windowDimensions = useWindowDimensions();
+
+  const mediaQ = useMediaQuery({ query: '(max-aspect-ratio: 1/1)' });
+
+  const scrollThresh = windowDimensions.height;
+  
+  const [scrollVal, api] = useSpring(() => ({scroll: 0}));
+
+  useScroll(({ xy: [, y] }) => 
+    api.start({
+      scroll: Math.min(Math.max(parseInt(y), 0), scrollThresh),
+    }),
+    { domTarget: window },
+  );
+
   return(
     <div className="About">
       <div className="AboutMeContainer">
-        <h2 className="AboutMeTitle">ABOUT ME</h2>
-        <div className="AboutMeContent">
-          <p>Hello. I'm Siddhi, a design focused frontend developer. With 10 years of graphic design experience, primarily focused on motion and layout design, I became interested in web and frontend development in the past year. </p>
-          <p>The primary draw for me is the interactivity and the UI/UX aspect of web, with the freedom to design, build, and create experiences without the restrictions of traditional media.</p>
+        <div className="AboutMeSticky">
+          <animated.h2 
+            className="AboutMeTitle"
+          >
+            ABOUT ME
+          </animated.h2>
+          <div className="AboutMeContent">
+            <p>Hello. I'm Siddhi, a design focused frontend developer. With 10 years of graphic design experience, primarily focused on motion and layout design, I became interested in web and frontend development during the pandemic. Motivated by a need at the company I worked for and free time to learn new things.</p>
+            <p>The aspect I enjoy the most about web design is the freedom to design, build, and create experiences without the restrictions of traditional media.</p>
+          </div>
         </div>
       </div>
     </div>
